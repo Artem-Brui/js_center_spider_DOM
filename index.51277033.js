9d9cd07140b93cf5ef55d035b7f7192d646c2526
Artem-Brui <artembryj@gmail.com>
2024-09-09 11:14:18 +0200

    make build

---
+++ index.51277033.js
@@ -0,0 +1,2 @@
+var e={wall:document.getElementsByClassName("wall")[0],spider:document.getElementsByClassName("spider")[0]},t=e.wall.clientHeight/2-e.spider.clientHeight/2,l=e.wall.clientWidth/2-e.spider.clientWidth/2;e.spider.style.top="".concat(t,"px"),e.spider.style.left="".concat(l,"px");
+//# sourceMappingURL=index.51277033.js.map
